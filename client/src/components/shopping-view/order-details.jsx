98aafb4f4b2602505d3f0b61c995ec78fc2bdaef
Papa-josh <nicojosh04@gmail.com>
2025-10-26 02:30:35 +0800

    feat: finish order list view and data fetching for user side

--- client/src/components/shopping-view/order-details.jsx
+++ client/src/components/shopping-view/order-details.jsx
@@ -3,9 +3,12 @@ import { Label } from "../ui/label";
 import { Separator } from "../ui/separator";
 import { DialogContent, DialogHeader, DialogTitle, DialogDescription } from "../ui/dialog";
 import { Badge } from "../ui/badge";
+import { useSelector } from "react-redux";
 
 
 function ShoppingOrderDetailsView({ orderDetails }) {
+  const {user} = useSelector(state => state.auth);
+
   console.log(orderDetails);
   return (
     <DialogContent className>
@@ -29,6 +32,14 @@ function ShoppingOrderDetailsView({ orderDetails }) {
             <p className="font-medium">Order Price</p>
             <Label>{orderDetails?.totalAmount}</Label>
           </div>
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">Payment Method</p>
+            <Label>{orderDetails?.paymentMethod}</Label>
+          </div>
+          <div className="flex mt-6 items-center justify-between">
+            <p className="font-medium">Payment Status</p>
+            <Label>{orderDetails?.paymentStatus}</Label>
+          </div>
           <div className="flex mt-2 items-center justify-between">
             <p className="font-medium">Order Status</p>
             <Label>
@@ -43,10 +54,16 @@ function ShoppingOrderDetailsView({ orderDetails }) {
           <div className="grid gap-2">
             <div className="font-medium">Order Details</div>
             <ul className="grid gap-3">
-              <li className="flex items-center justify-between">
-                <span>Product One</span>
-                <span>$100</span>
-              </li>
+              {
+                orderDetails?.cartItems && orderDetails?.cartItems.length > 0 ?
+                orderDetails?.cartItems.map((item) => (
+                  <li key={item._id} className="flex items-center justify-between">
+                    <span>Title: {item.title} (x{item.quantity})</span>
+                    <span>Quantity: {item.quantity}</span>
+                    <span>Price: ${item.price}</span>
+                  </li>
+                )) : null
+              }
             </ul>
           </div>
         </div>
@@ -54,12 +71,12 @@ function ShoppingOrderDetailsView({ orderDetails }) {
           <div className="grid gap-2">
             <div className="font-medium">Shipping Info</div>
             <div className="grid gap-0.5 text-muted-foreground">
-              <span>John Doe</span>
-              <span>Address</span>
-              <span>City</span>
-              <span>Pincode</span>
-              <span>Phone</span>
-              <span>Notes</span>
+              <span>{user?.name}</span>
+              <span>{orderDetails?.addressInfo?.address}</span>
+              <span>{orderDetails?.addressInfo?.city}</span>
+              <span>{orderDetails?.addressInfo?.pincode}</span>
+              <span>{orderDetails?.addressInfo?.phone}</span>
+              <span>{orderDetails?.addressInfo?.notes}</span>
             </div>
           </div>
         </div>
