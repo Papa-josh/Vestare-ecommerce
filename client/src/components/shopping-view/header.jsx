43504bee6c79f056668ea3b948267bd0b56e8ebe
Papa-josh <nicojosh04@gmail.com>
2025-12-01 07:10:53 +0800

    feat: show cart item count next to cart icon

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -95,8 +95,10 @@ function HeaderRightContent() {
           onClick={() => setOpenCartSheet(true)}
           variant="outline"
           size="icon"
+          className="relative"
         >
           <ShoppingCart className="h-6 w-6" />
+          <span className="absolute top-[-5px] right-[2px] font-bold text-sm" >{cartItems?.items?.length || 0}</span> 
           <span className="sr-only">User cart</span>
         </Button>
         <UserCartWrapper
